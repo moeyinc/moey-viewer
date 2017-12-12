a1154923a9ea7802b55d16e891f83b2daa75f195
Moey Design <5884seiya@gmail.com>
2017-12-12 16:12:16 -0500

    node env test

--- backend/bin/serialport-config.js
+++ backend/bin/serialport-config.js
@@ -1,11 +1,14 @@
 var config
+console.log('environment is ' + process.env.ENV)
 if (process.env.ENV === 'MOEY_SEIYA') {
+  console.log('moey seiya')
   config = {
     BAUDRATE: 115200,
     SERIAL_PATH_A: '/dev/tty.usbmodem3533221',
     SERIAL_PATH_B: '/dev/tty.usbmodem3533231'
   }
 } else if (process.env.ENV === 'SURFACE') {
+  console.log('surface')
   config = {
     BAUDRATE: 115200,
     SERIAL_PATH_A: 'COM4',
