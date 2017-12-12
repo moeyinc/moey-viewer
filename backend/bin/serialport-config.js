1470329e9e0433a5e493c64141ff6ffd9636696c
Moey Design <5884seiya@gmail.com>
2017-12-12 15:58:38 -0500

    node env

--- backend/bin/serialport-config.js
+++ backend/bin/serialport-config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  BAUDRATE: 115200,
-  SERIAL_PATH_A: '/dev/tty.usbmodem3533221',
-  SERIAL_PATH_B: '/dev/tty.usbmodem3533231'
+var config
+if (process.env.ENV === 'MOEY_SEIYA') {
+  config = {
+    BAUDRATE: 115200,
+    SERIAL_PATH_A: '/dev/tty.usbmodem3533221',
+    SERIAL_PATH_B: '/dev/tty.usbmodem3533231'
+  }
+} else if (process.env.ENV === 'SURFACE') {
+  config = {
+    BAUDRATE: 115200,
+    SERIAL_PATH_A: 'COM4',
+    SERIAL_PATH_B: 'COM3'
+  }
 }
+
+module.exports = config
