3d8475b745fbb658e4853de81275e227c71d3b12
Moey Design <5884seiya@gmail.com>
2018-02-09 15:57:29 -0500

    electronize

--- backend/bin/serialport-config.js
+++ backend/bin/serialport-config.js
@@ -14,6 +14,13 @@ if (process.env.ENV === 'MOEY_SEIYA') {
     SERIAL_PATH_A: 'COM4',
     SERIAL_PATH_B: 'COM3'
   }
+} else {
+  console.log('default')
+  config = {
+    BAUDRATE: 115200,
+    SERIAL_PATH_A: '/dev/tty.usbmodem3533221',
+    SERIAL_PATH_B: '/dev/tty.usbmodem3533231'
+  }
 }
 
 module.exports = config
